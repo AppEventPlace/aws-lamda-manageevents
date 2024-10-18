02b6e98972831095749820ca35a111ab587bb15a
Jeison Visbal <visbalgomez@gmail.com>
2024-10-18 14:09:06 -0500

    cambio prueba

--- aws-lambda-manageevents/src/createEvent.js
+++ aws-lambda-manageevents/src/createEvent.js
@@ -1,81 +1,84 @@
 const { v4 } = require("uuid");
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.createEvent = async (event) => {
-    let response;
+  let response;
 
-    try {
-        // Parsear el cuerpo de la solicitud
-        const { nombre, descripcion, fecha, ubicacion, organizador } = JSON.parse(event.body);
+  try {
+    // Parsear el cuerpo de la solicitud
+    const { nombre, descripcion, fecha, ubicacion, organizador, userId } =
+      JSON.parse(event.body);
 
-        if (!nombre || !descripcion || !fecha || !ubicacion || !organizador) {
-            throw new Error("Todos los campos son obligatorios");
-        }
-
-        // Generar un ID único para el evento
-        const eventoId = v4();
-        const createDate = new Date().toISOString();
-        const rquid = v4();
-        const id = rquid;
-
-        const newEvent = {
-            id,
-            eventoId,
-            nombre,
-            descripcion,
-            fecha,
-            ubicacion,
-            organizador,
-            createDate
-        };
-
-        // Intentar insertar el nuevo evento en DynamoDB
-        await dynamodb.put({
-            TableName: 'Eventos',
-            Item: newEvent,
-        }).promise();
-
-        // Respuesta exitosa
-        const statusDesc = "Evento creado exitosamente";
-        response = {
-            statusCode: 201,
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                statusDesc,
-                statusCode: 201,
-                eventoId,
-                createDate
-            }),
-        };
-
-    } catch (error) {
-        console.error("Error al crear el evento:", error);
-
-        // Manejo de errores
-        let errorMessage = "Error interno del servidor";
-        let errorDescription = "";
-        let statusCode = 500;
-
-        if (error.message === "Todos los campos son obligatorios") {
-            errorMessage = error.message;
-            statusCode = 400;
-        }
-
-        response = {
-            statusCode,
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                statusDesc: errorMessage,
-                statusMessage: errorDescription,
-                statusCode,
-            }),
-        };
+    if (!nombre || !descripcion || !fecha || !ubicacion || !organizador) {
+      throw new Error("Todos los campos son obligatorios");
     }
 
-    return response;
+    // Generar un ID único para el evento
+    const eventoId = v4();
+    const createDate = new Date().toISOString();
+    const rquid = v4();
+    const id = rquid;
+
+    const newEvent = {
+      id,
+      eventoId,
+      nombre,
+      descripcion,
+      fecha,
+      ubicacion,
+      organizador,
+      createDate,
+      userId,
+    };
+
+    // Intentar insertar el nuevo evento en DynamoDB
+    await dynamodb
+      .put({
+        TableName: "Eventos",
+        Item: newEvent,
+      })
+      .promise();
+
+    // Respuesta exitosa
+    const statusDesc = "Evento creado exitosamente";
+    response = {
+      statusCode: 201,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        statusDesc,
+        statusCode: 201,
+        eventoId,
+        createDate,
+      }),
+    };
+  } catch (error) {
+    console.error("Error al crear el evento:", error);
+
+    // Manejo de errores
+    let errorMessage = "Error interno del servidor";
+    let errorDescription = "";
+    let statusCode = 500;
+
+    if (error.message === "Todos los campos son obligatorios") {
+      errorMessage = error.message;
+      statusCode = 400;
+    }
+
+    response = {
+      statusCode,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        statusDesc: errorMessage,
+        statusMessage: errorDescription,
+        statusCode,
+      }),
+    };
+  }
+
+  return response;
 };
